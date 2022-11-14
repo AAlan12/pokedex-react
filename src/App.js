6fd057c7567e999bedb9b68776969eebaea4aa64
Antonio Alan <tonio.alan07@gmail.com>
2022-11-14 10:06:31 -0300

    componentes de pokemoncard e navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 import { Home } from './pages/Home';
+import Navbar from './components/Navbar';
+import PokemonCard from './components/PokemonCard';
+import { Container } from '@mui/system';
+import { Grid } from '@mui/material';
 
 function App() {
   return (
-   <Home/>
+   <div>
+    <Navbar/>
+    <Container maxWidth="false">
+      <Grid container>
+        <Grid item xs={3}>
+          <PokemonCard/>
+        </Grid>
+        <Grid item xs={3}>
+          <PokemonCard/>
+        </Grid>
+        <Grid item xs={3}>
+          <PokemonCard/>
+        </Grid>
+        <Grid item xs={3}>
+          <PokemonCard/>
+        </Grid>
+      </Grid>
+      
+      
+    </Container>
+   </div>
+   
   );
 }
 
