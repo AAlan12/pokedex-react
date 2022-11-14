2edc55537b64f1bee8f8900ab8379d6eaf8959ba
Antonio Alan <tonio.alan07@gmail.com>
2022-11-14 15:14:19 -0300

    captura de dados da API com axios

--- src/App.js
+++ src/App.js
@@ -1,35 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import { Home } from './pages/Home';
-import Navbar from './components/Navbar';
-import PokemonCard from './components/PokemonCard';
-import { Container } from '@mui/system';
-import { Grid } from '@mui/material';
+
 
 function App() {
   return (
-   <div>
-    <Navbar/>
-    <Container maxWidth="false">
-      <Grid container>
-        <Grid item xs={3}>
-          <PokemonCard/>
-        </Grid>
-        <Grid item xs={3}>
-          <PokemonCard/>
-        </Grid>
-        <Grid item xs={3}>
-          <PokemonCard/>
-        </Grid>
-        <Grid item xs={3}>
-          <PokemonCard/>
-        </Grid>
-      </Grid>
-      
-      
-    </Container>
-   </div>
-   
+   <Home/>
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import PokemonCard from "../components/PokemonCard";
+import { Container } from "@mui/system";
+import { Grid } from "@mui/material";
+import axios from "axios";
 
 export const Home = () => {
-    return(
-        <div>Home</div>
-    )
-}
+    const [pokemons, setPokemons] = useState([])
+    useEffect(() => {
+        getPokemons();
+    }, []);
+    const getPokemons = () => {
+        axios
+        .get("https://pokeapi.co/api/v2/pokemon?limit=100")
+        .then((res) => setPokemons(res.data.results))
+        .catch((err) => console.log(err));
+    };
+  return (
+    <div>
+      <Navbar />
+      <Container maxWidth="false">
+        <Grid container>
+            {pokemons.map((pokemon) =>(
+                <Grid item xs={3}>
+                    <PokemonCard />
+                </Grid>
+            ))}           
+        </Grid>
+      </Container>
+    </div>
+  );
+};
